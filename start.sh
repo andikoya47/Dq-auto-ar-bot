eef43f71a498286e4a02ce7b2557498a7f76c211
Arshad-ek2 <130440778+Arshad-ek2@users.noreply.github.com>
2023-04-22 11:47:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  https://github.com/Arshad-ek2/Dq-auto-ar-bot.git /Dq-auto-ar-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /Dq-auto-ar-bot
 fi
 cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
