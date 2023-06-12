03bff03a7bc0406cac4e3dbeaad547b442ca187a
Andi Koya <130440778+Arshad-ek2@users.noreply.github.com>
2023-06-12 19:44:12 +0530

    Remove all send btn

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1568,8 +1568,8 @@ async def auto_filter(client, msg, spoll=False):
         )
 
     btn.insert(0, [
-        InlineKeyboardButton("! Sᴇɴᴅ Aʟʟ Tᴏ PM !", callback_data=f"send_fall#{pre}#{0}#{message.from_user.id}"),
-        InlineKeyboardButton("! Lᴀɴɢᴜᴀɢᴇs !", callback_data=f"select_lang#{message.from_user.id}")
+        
+        InlineKeyboardButton("🀄 Lᴀɴɢᴜᴀɢᴇs 🀄", callback_data=f"select_lang#{message.from_user.id}")
     ])
 
     
