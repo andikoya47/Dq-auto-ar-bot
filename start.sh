121bfd9ae2421bd68d4579da9d6d1c6584c09fc2
Andi Koya <130440778+Arshad-ek2@users.noreply.github.com>
2023-07-15 08:27:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Dq-auto-ar-bot
 fi
-cd /DQ-The-File-Donor
+cd /Dq-auto-ar-bot
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
