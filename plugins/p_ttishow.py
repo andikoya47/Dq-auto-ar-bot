edf222eceae91ab8343cfe5943d9bb398241d0fd
Andi Koya <130440778+andikoya47@users.noreply.github.com>
2024-02-06 19:06:49 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, ChatType, Filters
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, MELCOW_NEW_USERS, MELCOW_VID, CHNL_LNK, GRP_LNK, ALW_GRP
 from database.users_chats_db import db
@@ -291,7 +291,7 @@ async def list_chats(bot, message):
             outfile.write(out)
         await message.reply_document('chats.txt', caption="List Of Chats")
 
-@Client.on_message(filters.chat_type.groups & filters.new_chat_members)
+@Client.on_message(Filters.chat(type=ChatType.GROUP) & Filters.new_chat_members)
 async def bye(client, message):
     try:
         if message.new_chat_members[0].id == client.get_me().id:
